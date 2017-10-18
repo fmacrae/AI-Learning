1d2b8b813946ef2873bf6c32af483d512aaa3a35
fmacrae <finlay.macrae@gmail.com>
2017-10-19 00:22:58 +0100

    Update autoShutdown.sh

--- autoShutdown.sh
+++ autoShutdown.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
+# Install instructions: stick this on your VM and if you run crontab and add it to your schedule like this:
+#  crontab -e
+#  * * * * * sh /home/finlay_macrae/autoShutdown.sh
+# Description:
+#   looks for established SSH connections then after two calls in a row where it sees no connections 
+#   it shuts down the server.  If you have something long running like training then you need to create 
+#   a file called keepRunning.txt in the same directory.  Add a line to error handling and end of execution 
+#   for your training script to delete the flag file once it crashes or completes.  
+1 Comment
+
 if [ -f keepRunning.txt ]; then
   echo 'flag to keep running is still there so no need to shutdown check'
   exit
